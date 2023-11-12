53fd7b4e9c5cba8813d928405623f5829bb25d99
Daniel <1732402421@qq.com>
2023-11-13 02:01:46 +0800

    Add files via upload
    This is a JavaScript file that helps to download TOEFL vocabulary audio from a specific website.

---
+++ TOEFLDownloader.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         HelloWorld
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  This is a test script.
+// @author       Daniel
+// @match        http://download.dogwood.com.cn//*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @license      MIT 
+// @require       https://cdn.bootcdn.net/ajax/libs/jquery/3.7.1/jquery.min.js
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // Your code here...
+  var download_button = $('<button>').attr({
+    text: 'Download',
+    target: '_blank',
+    href: $(location).attr('href')
+  }
+  )
+})();
