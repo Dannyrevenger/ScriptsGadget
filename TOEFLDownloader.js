d78013170935e442dc5f2b88a2fe989d12bc2414
Dannyrevenger <1732402421@qq.com>
2023-11-13 02:54:34 +0800

    change description

--- TOEFLDownloader.js
+++ TOEFLDownloader.js
@@ -2,13 +2,13 @@
 // @name         TOEFLDownloader
 // @namespace    http://tampermonkey.net/
 // @version      0.2
-// @description  This is a test script.
+// @description  This is a JavaScript file that helps to download TOEFL vocabulary audio from a specific website.
 // @author       Daniel
 // @match        http://download.dogwood.com.cn//*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @license      MIT 
-// @require       https://cdn.bootcdn.net/ajax/libs/jquery/3.7.1/jquery.min.js
+// @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.7.1/jquery.min.js
 // ==/UserScript==
 
 (function () {
