2e34faff6bf99ae6d82c377b4c9d233754b33fea
Dannyrevenger <1732402421@qq.com>
2023-11-16 16:45:10 +0800

    Fullfill by DOM

--- TOEFLDownloader.js
+++ TOEFLDownloader.js
@@ -1,24 +1,21 @@
 // ==UserScript==
 // @name         TOEFLDownloader
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      3.3
 // @description  This is a JavaScript file that helps to download TOEFL vocabulary audio from a specific website.
 // @author       Daniel
-// @match        *//download.dogwood.com.cn/*
+// @match        *://*.dogwood.*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @license      MIT 
 // @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.7.1/jquery.min.js
 // ==/UserScript==
+window.onload = function () {
+  const a = document.createElement('a')
+  a.href = $(location).attr('href')
+  a.download = a.href.split('/').pop()
+  document.body.appendChild(a)
+  a.click()
+  document.body.removeChild(a)
+}
 
-(function () {
-  'use strict';
-  alert('w')
-  // Your code here...
-  $('<button>').attr({
-    text: 'Download',
-    target: '_blank',
-    href: $(location).attr('href')
-  }
-  )
-})();
